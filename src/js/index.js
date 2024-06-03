63c4ad2a2e97bd6c17b2a6a7a270099100345776
Daniel Possamai <daniel.possamai20@gmail.com>
2024-06-03 14:33:57 -0300

    removido muito codigo desnecessario

--- src/js/index.js
+++ src/js/index.js
@@ -1,16 +1,16 @@
-let btnTheme = document.getElementById('switch');
-let meuHtml = document.querySelector('html');
-let profileImg = document.querySelector('#profile img');
+// let btnTheme = document.getElementById('switch');
+// let meuHtml = document.querySelector('html');
+// let profileImg = document.querySelector('#profile img');
 
-// meuHtml.classList.toggle('light');
+// // meuHtml.classList.toggle('light');
 
-btnTheme.addEventListener('click', () => {
-    meuHtml.classList.toggle('light');
+// btnTheme.addEventListener('click', () => {
+//     meuHtml.classList.toggle('light');
 
-    if(meuHtml.classList.contains('light')){
-        profileImg.src = './src/images/imgPerson.jpeg';
-    }else{
-        profileImg.src = './src/images/imgPerson.jpeg'
-    }
-});
+//     if(meuHtml.classList.contains('light')){
+//         profileImg.src = './src/images/imgPerson.jpeg';
+//     }else{
+//         profileImg.src = './src/images/imgPerson.jpeg'
+//     }
+// });
 
